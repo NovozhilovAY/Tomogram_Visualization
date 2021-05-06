c7fa325bdf1fa1c2452e1cc9f22a6ca01c65a530
Alexandr <sasha.n191@yandex.ru>
2021-05-06 08:52:16 +0300

    add Bin class

--- Tomogram_Visualization/Form1.cs
+++ Tomogram_Visualization/Form1.cs
@@ -16,5 +16,10 @@ namespace Tomogram_Visualization
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bin bin = new Bin();
+        }
     }
 }
