4f1da9a1f563178e264fcc9e48b5e0bbf5820a0c
Alexandr <sasha.n191@yandex.ru>
2021-05-06 09:07:01 +0300

    add View class

---
+++ Tomogram_Visualization/View.cs
@@ -0,0 +1,21 @@
+﻿using OpenTK.Graphics.ES10;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tomogram_Visualization
+{
+    class View
+    {
+        public void SetupView(int width, int height)
+        {
+            GL.ShadeModel((All)ShadingModel.Smooth);
+            GL.MatrixMode((All)MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(0, Bin.X, 0, Bin.Y, -1, 1);
+            GL.Viewport(0, 0, width, height);
+        }
+    }
+}
